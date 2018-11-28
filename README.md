542f093462d29d88fdb29c5f176492287255238f
longjinbing <longjinbin@yeah.net>
2018-11-28 15:38:26 +0800

    2018/11/28

---
+++ README.md
@@ -0,0 +1,10 @@
+大学生英语评价系统
+* spring mvc
+* mybatis
+* redis
+* shiro
+* vue
+* bootstrap
+* Jquery
+
+
--- platform-common/src/main/java/com/ljb/utils/DealMapValueHelper.java
+++
@@ -1,7 +0,0 @@
-package com.ljb.utils;
-
-import java.util.Map;
-
-public interface DealMapValueHelper {
-	void dealValue(String key, Map<String, Object> map);
-}
